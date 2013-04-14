e044bad8adda7ffa401463c8bf4239e80f16a575
Greg Maza <mazag52@gmail.com>
2013-04-13 22:20:27 -0500

    Original Rails Files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Artuts::Application.config.secret_token = 'ecaa940680df2e49476f9bf37ad56dd1ff94eea9250b8b1201a1999f68c009846c2cd92db62446215238f3e8c786677cb29214481d632ca8cb26747331e8aedf'
